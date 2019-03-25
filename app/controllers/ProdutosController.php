864f790b6f21af1720e6791e5b0c2ca1e99ded01
diego.dona <diego.dona@outlook.com.br>
2019-03-25 11:45:57 -0300

    produtos

--- app/controllers/ProdutosController.php
+++ app/controllers/ProdutosController.php
@@ -5,12 +5,21 @@ class ProdutosController extends ControllerBase
 
     public function indexAction()
     {
-        echo "shit";
+        $this->view->produtos = $this->getProdutos();
     }
 
     public function getAction()
     {
         
+    }
+
+    private function getProdutos()
+    {
+        return [
+            new Produto(1, "Laranja", 3.25),
+            new Produto(2, "Maçã", 1.25),
+            new Produto(3, "Orocuã", 3.5),
+        ];
     }
 
 }
---
+++ app/models/Produto.php
@@ -0,0 +1,17 @@
+<?php 
+
+use Phalcon\Mvc\Model;
+
+class Produto
+{
+    public $id;
+    public $nome;
+    public $preco;
+
+    public function __construct($id, $nome, $preco)
+    {
+        $this->id = $id;
+        $this->nome = $nome;
+        $this->preco = $preco;
+    }
+}
