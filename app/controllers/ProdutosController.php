2844d1dc06d3e77144104823d371949cd1770003
diego.dona <diego.dona@outlook.com.br>
2019-03-25 11:00:12 -0300

    primeiro versionamento

---
+++ app/controllers/ProdutosController.php
@@ -0,0 +1,17 @@
+<?php
+
+class ProdutosController extends ControllerBase
+{
+
+    public function indexAction()
+    {
+        echo "shit";
+    }
+
+    public function getAction()
+    {
+        
+    }
+
+}
+
